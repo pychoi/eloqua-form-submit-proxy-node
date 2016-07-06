425eaa906eebb7cf6a7df2091f07dbf794b78992
Pui Choi <puichoi@Puis-MacBook-Air.local>
2016-07-06 11:54:45 -0500

    remove index references

--- server/app.js
+++ server/app.js
@@ -4,18 +4,16 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var request = require('request');
 
+app.set('port', process.env.PORT || 5000);
+
 app.use(function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
-app.set('port', process.env.PORT || 5000);
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({expanded: true}));
-
-app.use('/', index);
 
 app.post('/', function(req, res, next){
     console.log(req.body);
