e44f5674a0992cd5f3a4cf5598d3290f8f9185ce
Pui Choi <puichoi@Puis-MacBook-Air.local>
2016-07-06 13:01:06 -0500

    correct setHeader

--- server/app.js
+++ server/app.js
@@ -6,9 +6,9 @@ var request = require('request');
 //var index = require('./index.js');
 
 app.use(function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    return next();
 });
 
 app.set('port', process.env.PORT || 5000);
